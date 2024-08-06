033e1975228aba947048cbb30656735db5cad136
Sujan Tamang <suzanyba079@gmail.com>
2024-08-06 19:25:28 +0545

    added 404 page with css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
         {/* Error route handel */}
         <Route path="*" element={<NotFound />} />
 
+        {/* Pages routing */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
